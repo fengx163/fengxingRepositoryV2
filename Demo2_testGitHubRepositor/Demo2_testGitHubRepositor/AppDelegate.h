cf0d9608b27d31c3d1be4bff16acb83b9d448895
fengxing <446981022@qq.com>
2015-12-01 10:06:53 +0800

    第一次推送

---
+++ Demo2_testGitHubRepositor/Demo2_testGitHubRepositor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo2_testGitHubRepositor
+//
+//  Created by tarena04_03 on 15/12/1.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
